4cae571a9f1f66e934c766f5df5f3e8c7690a917
wisky <maxim.ottevaere@student.howest.be>
2017-09-28 09:47:32 +0200

    fqsfqfqf

---
+++ opdracht 1.py
@@ -0,0 +1,5 @@
+basis = float(input ("geef de basis van een driehoek :"))
+hoogte = input ("geef de hoogte : ")
+hoogte = float(hoogte)
+oppervlakte = basis + hoogte /2
+print ("de oppervlakte van de driehoek is {0}".format(oppervlakte))
